e9b27168e57e5aea159e22e04d87a7db42142481
Xiaosheng Liu <maxlxs19951103@gmail.com>
2020-03-11 17:38:51 -0400

    TopNavBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './nav_logoman.svg';
 import './App.css';
+import { TopNavBar } from './TopNavBar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-
-      </header>
+      <TopNavBar />
     </div>
   );
 }
---
+++ src/TopNavBar.js
@@ -0,0 +1,12 @@
+import logo from "./nav_logoman.svg";
+import React from "react";
+
+export function TopNavBar(props){
+    return(
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    );
+
+}
+
